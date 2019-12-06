9403a291a18f7727ad9b3c052ca58aff822b97eb
kodai920 <kodaiokaoka0920@gmail.com>
2019-12-06 13:49:12 +0530

    heroku migrate

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->sentence(3),
-        'image' => secure_asset('uploads/products/sample.jpg'),
+        'image' => secure_asset('uploads/products/sample.jpeg'),
         'description' => $faker->paragraph(4),
         'price' => $faker->numberBetween(100,10000)
     ];
